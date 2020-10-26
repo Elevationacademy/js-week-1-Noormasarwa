d7bd0238f910e764e145b6f69f49f2ddf13c4437
Noor Massarwe <noor_19771@outlook.com>
2020-10-26 20:11:59 +0200

    solving and reverting to the older version

---
+++ third-task/main.js
@@ -0,0 +1,197 @@
+/*const maxCapacity = 14;
+let surfTime = true;
+let bestStudent ;
+const purposeInLife;
+
+console.log(maxCapacity);
+console.log(surfTime);
+console.log(bestStudent);
+console.log(purposeInLife);*/
+
+
+/*
+const myVariable = "The potential of the future "
+const result = myVariable + "is the best thing ever"
+console.log(result)*/
+
+
+/*
+const password = "Aubida Is the best"
+const confirmPassword = "loool"
+
+if(password === confirmPassword){
+    console.log("password confirmed");
+}
+else{
+    console.log("password are not the same");
+}
+*/
+/*
+
+const math1 = 423 *12;
+const math2 = 802/2;
+const math3 = (5+6)*3;
+
+console.log(math1);
+console.log(math2);
+console.log(math3);
+*/
+/*
+if((5 > 2) && false){
+    console("(5 > 2) && false"+" truthy");
+     }else{
+         console.log("(5 > 2) && false"+" falsy");
+         }
+
+if(!("knife" === "sword")){
+    console.log("!(knife === sword)"+" truthy");
+    }else{
+        console.log("!(knife === sword)"+" falsy");
+       }
+
+ if((1 < 2) || (-1 > -1) || !false){
+             console.log("(1 < 2) || (-1 > -1) || !false"+" truthy");
+        }else{
+         console.log("(1 < 2) || (-1 > -1) || !false"+" falsy");
+ }
+
+if(""){
+console.log("\"\""+" truthy");
+    }else{
+        console.log("\"\""+" falsy");
+}
+
+if((31 % 5) == "1"){
+    console.log("(31 % 5) == 1"+" truthy");
+        }else{
+            console.log("(31 % 5) == 1"+" falsy");
+}
+
+if(!!true){
+    console.log("!!true"+" truthy");
+        }else{
+   console.log("!!true "+" falsy");
+}
+
+if("5th Avenue" != "5th Avenue"){
+    console.log("5th Avenue != 5th Avenue"+" truthy");
+        }else{
+    console.log("5th Avenue != 5th Avenue"+" falsy");
+}
+
+if(52 !== "52"){
+    console.log("52 !== \"52\""+" truthy");
+        }else{
+   console.log("52 !== \"52\" "+" falsy");
+}
+
+if((undefined || null)){
+    console.log("(undefined || null)"+" truthy");
+        }else{
+   console.log("(undefined || null) "+" falsy");
+}
+
+*/
+/*
+let a = 3
+a = 4
+let c = 0
+let b = a
+b = 2
+a = b
+b = c
+c = a
+a = b
+
+console.log("a is "+ a);
+console.log("b is "+ b);
+console.log("c is "+ c);
+
+*/
+
+/*
+let input = prompt("inset your age");
+console.log(input); 
+*/
+/*
+let numChildren = prompt("inset the number of your Children:")
+const isCareful = prompt("is Carful:")
+
+if(!isCareful){
+    numChildren++;
+}
+
+console.log(numChildren)
+*/
+
+/*
+let silverwareCount = prompt("inset the value of your silverwareCount:")
+if(silverwareCount %2 == 0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+
+*/
+/*
+if(performance == "good") {
+    salary += goodBonus
+} else if(performance == "stellar") {
+  salary += stellarBonus
+}*/
+
+/*const isVIP = false
+let cash = 500
+
+if(isVIP || cash > 300){
+  console.log("Let me wipe your shoes before you go in")
+} else {
+  console.log("Hmph.")
+}*/
+
+/*
+const a = 3
+let b = 2
+let c = 0 || 12
+let d
+
+b = c
+b++
+if(d){
+  b = a
+}
+
+d = a + (b * c)
+d++
+b += 2
+*/
+
+/*
+
+if (gender == "male") {
+  profession += "man"
+} else if (gender == "female") {
+  proession += "woman"
+} else {
+  profession += "person"
+}
+
+console.log(profession)
+*/
+
+/*
+
+if (boughtTesla && isUSCitizen) {
+  if(currentYear - yearOfTeslaPurchase >= 4) {
+    console.log("Would you like an upgrade")
+  } else {
+    console.log("Are you satisfied with your purchase")
+  }
+} else if (boughtTesla) {
+  console.log("Would you like to move to the US?")
+} else if (!boughtTesla) {
+  console.log("Are you interested in purchasing a Tesla")
+} 
+
+*/
